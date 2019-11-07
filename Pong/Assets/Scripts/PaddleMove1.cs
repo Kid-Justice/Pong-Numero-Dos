6347c695dca120afe6e3ab002566958848f305b6
Kid-Justice <drew.jennings@digipen.edu>
2019-11-06 17:59:45 -0800

    Added Paddles and Divider

---
+++ Pong/Assets/Scripts/PaddleMove1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMove1 : MonoBehaviour
+{
+    public float Speed = 0.25f;
+    public float Edge = 3.15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) && transform.position.y < Edge)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + Speed, transform.position.z);
+            if (transform.position.y > Edge)
+            {
+                transform.position = new Vector3(transform.position.x, Edge, transform.position.z);
+            }
+        }
+        if (Input.GetKey(KeyCode.S) && transform.position.y > -Edge)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - Speed, transform.position.z);
+            if (transform.position.y < -Edge)
+            {
+                transform.position = new Vector3(transform.position.x, -Edge, transform.position.z);
+            }
+        }
+    }
+}
---
+++ Pong/Assets/Scripts/PaddleMove2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMove2 : MonoBehaviour
+{
+    public float Speed = 0.25f;
+    public float Edge = 3.15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) && transform.position.y < Edge)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + Speed, transform.position.z);
+            if (transform.position.y > Edge)
+            {
+                transform.position = new Vector3(transform.position.x, Edge, transform.position.z);
+            }
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && transform.position.y > -Edge)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - Speed, transform.position.z);
+            if (transform.position.y < -Edge)
+            {
+                transform.position = new Vector3(transform.position.x, -Edge, transform.position.z);
+            }
+        }
+    }
+}
