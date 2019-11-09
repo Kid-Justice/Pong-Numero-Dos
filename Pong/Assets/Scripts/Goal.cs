75c57ae66d3d9f29409c4cf8f32baec211ef15ff
Kid-Justice <drew.jennings@digipen.edu>
2019-11-08 16:12:06 -0800

    Pushing it cause Hugo is burning down the forest

---
+++ Pong/Assets/Scripts/Goal.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public int Player = 1;
+    public GameObject GMObject;
+    GameManager GM;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GM = GMObject.GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Ball BallScript = collision.gameObject.GetComponent<Ball>();
+        if (BallScript != null)
+        {
+            switch (Player)
+            {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+            }
+        }
+        Destroy(collision.gameObject);
+    }
+}
